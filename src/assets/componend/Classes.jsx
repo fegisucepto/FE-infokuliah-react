698d9df9991222f439efeb0f989ca6436c70c72c
Fegi Sucepto Priawan <fegisucepto6997@gmail.com>
2023-12-09 02:22:55 +0700

    update course

--- src/assets/componend/Classes.jsx
+++ src/assets/componend/Classes.jsx
@@ -20,9 +20,15 @@ export default function Classe() {
       image: ptnImage,
       description: "Program Garansi STAN/Kedinasan dengan sistem pembelajaran privat maupun group class. Siswa belajar dengan kondisi professional, fokus terhadap tujuan dan pengajar pendamping yang kompeten untuk persiapan ujian masuk STAN."
     },
+    {
+      title: "Program Masuk PTN",
+      image: ptnImage,
+      description: "Program Garansi STAN/Kedinasan dengan sistem pembelajaran privat maupun group class. Siswa belajar dengan kondisi professional, fokus terhadap tujuan dan pengajar pendamping yang kompeten untuk persiapan ujian masuk STAN."
+    },
   ];
+
   return (
-<div className="container-fluid pt-5 pb-5 bg-light">
+    <div className="container-fluid pt-5 pb-5 bg-light">
       <div className="container pt-5 pb-5">
         <div className="text-center pb-2">
           <p className="section-title px-5">
@@ -32,7 +38,7 @@ export default function Classe() {
         </div>
         <div className="row-clases">
           {programsClass.map((program, index) => (
-            <div key={index} className="col-md-6 col-lg-3 text-center mb-5">
+            <div key={index} className="card-container">
               <div className="card border-0 bg-light shadow-sm pb-2">
                 <img className="card-img-top mb-2" src={program.image} alt="" />
                 <div className="card-body text-center">
